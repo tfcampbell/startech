a36f6018ac129f62e4d0b33ef690562e3f1d6948
tfcampbell <49284478+tfcampbell@users.noreply.github.com>
2020-05-15 15:49:24 +0100

    Add files via upload

---
+++ Item.py
@@ -0,0 +1,24 @@
+class Item():
+    
+    def __init__(self):
+        self.name = None
+        self. description = None
+        
+    def set_item_name(self, item_name):
+        self.name = item_name
+        
+    def get_item_name(self):
+        return self.item_name
+        
+    def set_description(self, item_description):
+        self.description = item_description
+        
+    def get_description(self):
+        return self.item_description
+    
+        
+        
+        
+        
+    
+    
